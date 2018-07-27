e268a5549735a6b7ca8c73ec6caedb23a4748ab5
pranav gupta <pg07codes@gmail.com>
2018-07-27 18:30:44 +0530

    fixed nav

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -3,22 +3,22 @@
 //smooth scroll code here-----------
 $('#link1').on('click',(e)=>{
     e.preventDefault()
-    let x=$('#what-we-do').offset().top;
+    let x=$('#what-we-do').offset().top - 63;
     $('body,html').animate({'scrollTop': x},500)
 })
 $('#link2').on('click',(e)=>{
     e.preventDefault()
-    let x=$('#who-can-join').offset().top;
+    let x=$('#who-can-join').offset().top  - 63;
     $('body,html').animate({'scrollTop': x},1000)
 })
 $('#link3').on('click',(e)=>{
     e.preventDefault()
-    let x=$('#blog').offset().top;
+    let x=$('#blog').offset().top  - 63;
     $('body,html').animate({'scrollTop': x},1500)
 })
 $('#link4').on('click',(e)=>{
     e.preventDefault()
-    let x=$('#connect').offset().top;
+    let x=$('#connect').offset().top  - 63;
     $('body,html').animate({'scrollTop': x},2000)
 })
 
