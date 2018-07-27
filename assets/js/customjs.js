5dc9c753cd57d904d34eefad6debdb75ba297f51
pranav gupta <pg07codes@gmail.com>
2018-07-27 17:30:57 +0530

    added smooth scroll

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -1,1 +1,25 @@
 /*created by Pranav Gupta (pg07codes) on 26-07-2018 */
+
+//smooth scroll code here-----------
+$('#link1').on('click',(e)=>{
+    e.preventDefault()
+    let x=$('#what-we-do').offset().top;
+    $('body,html').animate({'scrollTop': x},500)
+})
+$('#link2').on('click',(e)=>{
+    e.preventDefault()
+    let x=$('#who-can-join').offset().top;
+    $('body,html').animate({'scrollTop': x},800)
+})
+$('#link3').on('click',(e)=>{
+    e.preventDefault()
+    let x=$('#blog').offset().top;
+    $('body,html').animate({'scrollTop': x},1200)
+})
+$('#link4').on('click',(e)=>{
+    e.preventDefault()
+    let x=$('#connect').offset().top;
+    $('body,html').animate({'scrollTop': x},1500)
+})
+
+//smooth scroll code ends here-----------
